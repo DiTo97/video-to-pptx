c3b53741065dea0f784aa5317ca8b1c7bfa0e6b8
f.minutoli <f.minutoli@reply.it>
2022-12-09 04:21:08 +0100

    Register pytest marks in conftest.py

--- src/video-to-pptx/tests/conftest.py
+++ src/video-to-pptx/tests/conftest.py
@@ -8,3 +8,9 @@ from video2pptx import ROOT
 @pytest.fixture
 def ROOT_tests() -> pathlib.Path:
     return ROOT.parents[1] / "tests"
+
+
+def pytest_configure(config: pytest.Config):
+    config.addinivalue_line("markers", "unit: A marker for unit tests")
+    config.addinivalue_line("markers", "integration: A marker for integration tests")
+    config.addinivalue_line("markers", "e2e: A marker for end-to-end tests")
